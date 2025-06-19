c642efe35a8378feff36c6f3c36148baf61cabc9
Jak Kolb Windows <fishinjak@gmail.com>
2025-06-18 21:53:29 -0400

    photo

--- src/data/artworks.js
+++ src/data/artworks.js
@@ -23,6 +23,30 @@ const artworks = [
     src: process.env.PUBLIC_URL + "/imgs/animals/Cat_1.jpeg",
     title: "Art 4",
     category: "animals",
+  },
+  {
+    id: 5,
+    src: process.env.PUBLIC_URL + "/imgs/cars/5FCAD356-CAE8-4177-9F4E-C26A704A55C8.heic",
+    title: "Art 5",
+    category: "cars",
+  },
+  {
+    id: 6,
+    src: process.env.PUBLIC_URL + "/imgs/cars/ED188192-FDDB-441D-A7E9-6DB1BC25D1E9.heic",
+    title: "Art 6",
+    category: "cars",
+  },
+  {
+    id: 7,
+    src: process.env.PUBLIC_URL + "/imgs/cars/0CFEE4FE-DB44-4300-B735-966EDFA755A9.heic",
+    title: "Art 7",
+    category: "cars",
+  },
+  {
+    id: 8,
+    src: process.env.PUBLIC_URL + "/imgs/animals/IMG_2979.PNG",
+    title: "Art 8",
+    category: "animals",
   }];
 
 export default artworks;
